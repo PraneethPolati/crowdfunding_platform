d3883e209743def55df12ca0e6f696af49b269bc
Praneeth Polati <polatipraneeth@gmail.com>
2025-03-01 11:07:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to Crowdfunding API");
 });
 
-// Connect to MongoDB (Replace with your actual MongoDB URI)
+// Connect to MongoDB 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
